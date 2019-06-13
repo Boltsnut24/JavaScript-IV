39277899bba8738c9c3773e3dd603d9164cab6a4
Brad <Brad@BRADSPC>
2019-06-13 15:40:45 -0600

    Added Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,24 @@
 // CODE here for your Lambda Classes
+class Person{
+  constructor(info){
+    this.name = info.name;
+    this.location = info.location;
+    this.age = info.age;
+  }
+  
+  speak(){
+    console.log(`Hello my name is ${this.name)', I am from '${this.location}`);
+  }
+}
+
+class Student extends Person{
+
+}
+
+class Instructors extends Person{
+
+}
+
+class ProjectManager extends Instructors{
+
+}
